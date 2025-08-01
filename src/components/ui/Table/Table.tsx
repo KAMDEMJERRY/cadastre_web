77cbc89af8f370922591dc29c3c7df9a3f1b28be
Ndombou jerry <ndomboukamdem@gmail.com>
2025-08-01 12:47:32 +0100

    feat: ajout des compoant button, input, card, form et label de shadcn

--- src/components/ui/Table/Table.tsx
+++
@@ -1,9 +0,0 @@
-import { ReactNode } from 'react';
-
-export default function Table({ children }: { children: ReactNode }) {
-  return (
-    <div className="bg-white rounded-lg shadow overflow-hidden">
-      <table className="w-full text-left">{children}</table>
-    </div>
-  );
-}
