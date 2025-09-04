898c1dbd58b26c6960565485b65298e5e2464e3e
Ndombou jerry <ndomboukamdem@gmail.com>
2025-09-04 13:59:55 +0100

    setting for deployment on vercel

--- next.config.ts
+++ next.config.ts
@@ -1,17 +1,42 @@
-import type { NextConfig } from "next";
+// import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
+// const nextConfig: NextConfig = {
+//   /* config options here */
   
-};
+// };
 
-module.exports = {
-  // ... other config
-  experimental: {
-    allowedDevOrigins: ["192.168.1.123", "localhost"] // Add all allowed origins
-  }
+// module.exports = {
+//   // ... other config
+//   experimental: {
+//     allowedDevOrigins: ["192.168.1.123", "localhost"] // Add all allowed origins
+//   }
 
 
+// }
+
+// export default nextConfig;
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // Configuration optimisée pour Vercel
+  images: {
+    unoptimized: true,
+    domains: ['https://cadastre-backend-3whj.onrender.com'],
+  },
+  env: {
+    API_URL: process.env.API_URL,
+  },
+  // Important pour les redirections et rewrites
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: `${process.env.NEXT_PUBLIC_API_URL}/api/:path*`,
+      },
+    ];
+  },
+  // Pour le support des monorepos (si applicable)
+  skipTrailingSlashRedirect: true,
 }
 
-export default nextConfig;
+module.exports = nextConfig
